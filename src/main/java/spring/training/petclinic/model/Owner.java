82f4ea58da05307fab5f3d1cf7a1235d97a200a6
Rodrigo Guerrero <rodrigo.guerrero@solera.com>
2022-09-26 19:45:15 +0200

    Add POJOs

---
+++ src/main/java/spring/training/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package spring.training.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/spring/training/petclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package spring.training.petclinic.model;
+
+public class Vet extends Person {
+
+
+}
